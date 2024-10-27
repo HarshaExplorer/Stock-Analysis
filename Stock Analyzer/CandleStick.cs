c3a5d219d4cda7704859e3d747c2b2cda47c5427
Harsha Yuvaraj <harshavardanyuvaraj@gmail.com>
2024-10-27 08:21:48 -0400

    CandleStick class created

---
+++ Stock Analyzer/CandleStick.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Analyzer
+{
+    internal class CandleStick
+    {
+        public DateTime Date { get; set; }
+        public decimal Open { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public decimal Close { get; set; }
+        public ulong Volume { get; set; }
+
+
+        public CandleStick(string dateStr, string openStr, string highStr, string lowStr, string closeStr, string volumeStr)
+        {
+
+        }
+        public override string ToString()
+        {
+            return $"Date: {Date.ToShortDateString()}, Open: {Open}, High: {High}, Low: {Low}, Close: {Close}, Volume: {Volume}";
+        }
+    }
+}
