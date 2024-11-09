a158bd579f09600dd6199462ce4c5ebec8e181d2
Harsha Yuvaraj <harshavardanyuvaraj@gmail.com>
2024-11-09 16:58:33 -0500

    New UI layout

--- Stock Analyzer/form_main.Designer.cs
+++ Stock Analyzer/form_main.Designer.cs
@@ -29,11 +29,10 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.groupBox_actionPanel = new System.Windows.Forms.GroupBox();
             this.dateTimePicker_endDate = new System.Windows.Forms.DateTimePicker();
             this.label_endDate = new System.Windows.Forms.Label();
@@ -41,13 +40,13 @@
             this.label_startDate = new System.Windows.Forms.Label();
             this.button_update = new System.Windows.Forms.Button();
             this.button_loadStocks = new System.Windows.Forms.Button();
-            this.candleStickBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.chart_OHLCV = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.openFileDialog_stockFilePick = new System.Windows.Forms.OpenFileDialog();
+            this.candleStickBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.formmainBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.groupBox_actionPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.candleStickBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart_OHLCV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.candleStickBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.formmainBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
@@ -64,7 +63,7 @@
             this.groupBox_actionPanel.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.groupBox_actionPanel.Name = "groupBox_actionPanel";
             this.groupBox_actionPanel.Padding = new System.Windows.Forms.Padding(8, 7, 8, 7);
-            this.groupBox_actionPanel.Size = new System.Drawing.Size(900, 465);
+            this.groupBox_actionPanel.Size = new System.Drawing.Size(3416, 189);
             this.groupBox_actionPanel.TabIndex = 0;
             this.groupBox_actionPanel.TabStop = false;
             this.groupBox_actionPanel.Text = "Action Panel";
@@ -73,7 +72,7 @@
             // 
             this.dateTimePicker_endDate.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker_endDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker_endDate.Location = new System.Drawing.Point(256, 358);
+            this.dateTimePicker_endDate.Location = new System.Drawing.Point(2173, 109);
             this.dateTimePicker_endDate.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.dateTimePicker_endDate.Name = "dateTimePicker_endDate";
             this.dateTimePicker_endDate.Size = new System.Drawing.Size(604, 41);
@@ -84,7 +83,7 @@
             // 
             this.label_endDate.AutoSize = true;
             this.label_endDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_endDate.Location = new System.Drawing.Point(16, 358);
+            this.label_endDate.Location = new System.Drawing.Point(2360, 47);
             this.label_endDate.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label_endDate.Name = "label_endDate";
             this.label_endDate.Size = new System.Drawing.Size(181, 38);
@@ -94,7 +93,7 @@
             // dateTimePicker_startDate
             // 
             this.dateTimePicker_startDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker_startDate.Location = new System.Drawing.Point(256, 258);
+            this.dateTimePicker_startDate.Location = new System.Drawing.Point(605, 109);
             this.dateTimePicker_startDate.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.dateTimePicker_startDate.Name = "dateTimePicker_startDate";
             this.dateTimePicker_startDate.Size = new System.Drawing.Size(604, 41);
@@ -105,7 +104,7 @@
             // 
             this.label_startDate.AutoSize = true;
             this.label_startDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_startDate.Location = new System.Drawing.Point(16, 258);
+            this.label_startDate.Location = new System.Drawing.Point(784, 47);
             this.label_startDate.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label_startDate.Name = "label_startDate";
             this.label_startDate.Size = new System.Drawing.Size(194, 38);
@@ -115,7 +114,7 @@
             // button_update
             // 
             this.button_update.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_update.Location = new System.Drawing.Point(472, 74);
+            this.button_update.Location = new System.Drawing.Point(1731, 35);
             this.button_update.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.button_update.Name = "button_update";
             this.button_update.Size = new System.Drawing.Size(395, 129);
@@ -127,7 +126,7 @@
             // button_loadStocks
             // 
             this.button_loadStocks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_loadStocks.Location = new System.Drawing.Point(16, 69);
+            this.button_loadStocks.Location = new System.Drawing.Point(1264, 30);
             this.button_loadStocks.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.button_loadStocks.Name = "button_loadStocks";
             this.button_loadStocks.Size = new System.Drawing.Size(395, 134);
@@ -136,42 +135,32 @@
             this.button_loadStocks.UseVisualStyleBackColor = true;
             this.button_loadStocks.Click += new System.EventHandler(this.button_loadStocks_Click);
             // 
-            // candleStickBindingSource
-            // 
-            this.candleStickBindingSource.DataSource = typeof(Stock_Analyzer.CandleStick);
-            // 
             // chart_OHLCV
             // 
-            chartArea3.BorderWidth = 2;
-            chartArea3.Name = "ChartArea_OHLC";
-            chartArea4.BorderWidth = 2;
-            chartArea4.Name = "ChartArea_Volume";
-            this.chart_OHLCV.ChartAreas.Add(chartArea3);
-            this.chart_OHLCV.ChartAreas.Add(chartArea4);
-            legend2.Alignment = System.Drawing.StringAlignment.Center;
-            legend2.LegendItemOrder = System.Windows.Forms.DataVisualization.Charting.LegendItemOrder.ReversedSeriesOrder;
-            legend2.Name = "Legend_OHLCV";
-            this.chart_OHLCV.Legends.Add(legend2);
-            this.chart_OHLCV.Location = new System.Drawing.Point(32, 549);
+            chartArea1.BorderWidth = 2;
+            chartArea1.Name = "ChartArea_OHLC";
+            chartArea2.BorderWidth = 2;
+            chartArea2.Name = "ChartArea_Volume";
+            this.chart_OHLCV.ChartAreas.Add(chartArea1);
+            this.chart_OHLCV.ChartAreas.Add(chartArea2);
+            this.chart_OHLCV.Location = new System.Drawing.Point(45, 244);
             this.chart_OHLCV.Name = "chart_OHLCV";
-            series3.ChartArea = "ChartArea_OHLC";
-            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
-            series3.CustomProperties = "PriceDownColor=Red, PriceUpColor=Lime";
-            series3.Legend = "Legend_OHLCV";
-            series3.Name = "Series_OHLC";
-            series3.XValueMember = "Date";
-            series3.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
-            series3.YValueMembers = "High,Low,Open,Close";
-            series3.YValuesPerPoint = 4;
-            series4.ChartArea = "ChartArea_Volume";
-            series4.Legend = "Legend_OHLCV";
-            series4.Name = "Series_Volume";
-            series4.XValueMember = "Date";
-            series4.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
-            series4.YValueMembers = "Volume";
-            this.chart_OHLCV.Series.Add(series3);
-            this.chart_OHLCV.Series.Add(series4);
-            this.chart_OHLCV.Size = new System.Drawing.Size(2942, 922);
+            series1.ChartArea = "ChartArea_OHLC";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Candlestick;
+            series1.CustomProperties = "PriceDownColor=Red, PriceUpColor=Lime";
+            series1.Name = "Series_OHLC";
+            series1.XValueMember = "Date";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
+            series1.YValueMembers = "High,Low,Open,Close";
+            series1.YValuesPerPoint = 4;
+            series2.ChartArea = "ChartArea_Volume";
+            series2.Name = "Series_Volume";
+            series2.XValueMember = "Date";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.DateTime;
+            series2.YValueMembers = "Volume";
+            this.chart_OHLCV.Series.Add(series1);
+            this.chart_OHLCV.Series.Add(series2);
+            this.chart_OHLCV.Size = new System.Drawing.Size(3403, 1474);
             this.chart_OHLCV.TabIndex = 2;
             this.chart_OHLCV.Text = "chart_OHLCV";
             // 
@@ -181,6 +170,10 @@
     "iles|*-Day.csv";
             this.openFileDialog_stockFilePick.Title = "Select a CSV File";
             this.openFileDialog_stockFilePick.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // candleStickBindingSource
+            // 
+            this.candleStickBindingSource.DataSource = typeof(Stock_Analyzer.CandleStick);
             // 
             // formmainBindingSource
             // 
@@ -193,18 +186,18 @@
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(3025, 1499);
+            this.ClientSize = new System.Drawing.Size(3491, 1751);
             this.Controls.Add(this.chart_OHLCV);
             this.Controls.Add(this.groupBox_actionPanel);
             this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "Form_Main";
-            this.Padding = new System.Windows.Forms.Padding(30);
+            this.Padding = new System.Windows.Forms.Padding(29, 31, 29, 31);
             this.Text = "Stock Viewer";
             this.Load += new System.EventHandler(this.form_main_Load);
             this.groupBox_actionPanel.ResumeLayout(false);
             this.groupBox_actionPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.candleStickBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart_OHLCV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.candleStickBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.formmainBindingSource)).EndInit();
             this.ResumeLayout(false);
 
