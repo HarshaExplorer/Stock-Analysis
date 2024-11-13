afcb44b6677af2f645b2d0ab7d22cd71140aa5d5
Harsha Yuvaraj <harshavardanyuvaraj@gmail.com>
2024-11-13 13:20:14 -0500

    PeakValleyDetector class created

---
+++ Stock Analyzer/PeakValleyDetector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_Analyzer
+{
+    internal class PeakValleyDetector
+    {
+        public List<int> Peaks {  get; set; }
+
+        public List<int> Valleys { get; set; }
+
+        public PeakValleyDetector(BindingList<CandleStick> candlesticks)
+        {
+            CandleStick prev, curr, next;
+
+            for (int i = 0; i < candlesticks.Count; i++)
+            {
+                prev = candlesticks[i-1];
+                curr = candlesticks[i];
+                next = candlesticks[i+1];
+
+                if (prev == null || curr == null || next == null)
+                    continue;
+
+                if (curr.High > prev.High && curr.High > next.High)
+                    Peaks.Add(i);
+
+                else if(curr.Low < prev.Low && curr.Low < next.Low)
+                    Valleys.Add(i);
+
+            }
+    }
+}
